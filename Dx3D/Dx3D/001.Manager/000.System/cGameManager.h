6659f2f83f31343a5801b958f123c1eedb5b16e6
Eugene Kim <komastar.dev@gmail.com>
2018-03-21 09:21:59 +0900

    add inventory structure

--- Dx3D/Dx3D/001.Manager/000.System/cGameManager.h
+++ Dx3D/Dx3D/001.Manager/000.System/cGameManager.h
@@ -3,6 +3,29 @@
 
 #define g_pGameManager cGameManager::GetInstance()
 
+struct ST_ITEM
+{
+    int id;
+    int count;
+
+    ST_ITEM()
+    {
+        id = -1;
+        count = -1;
+    }
+};
+
+struct ST_INVENTORY
+{
+    int     gold;
+    vector<ST_ITEM> items;
+
+    ST_INVENTORY()
+    {
+        gold = 0;
+    }
+};
+
 class cGameManager : public iSingletonManager
 {
 private:
@@ -26,5 +49,10 @@ public:
     bool IsLoadData() { return m_isLoadData; }
     void DisableLoadFlag() { m_isLoadData = false; }
     void EnableLoadFlag() { m_isLoadData = true; }
+
+    void PushItem(int id);
+    void PullItem(int id);
+
+    void Pay(int gold);
 };
 
